19aa256b74c8f6ed10bf5db1cb8b1d3b229ac26e
Ralph <rdugue1@gmail.com>
2026-03-25 23:51:46 -0400

    policy evaluation

--- reinforcement-learning/policy_evaluation/monte_carlo.py
+++ reinforcement-learning/policy_evaluation/monte_carlo.py
@@ -24,3 +24,31 @@ class MonteCarlo:
 
         V = np.divide(g_sums, g_counts, out=np.zeros_like(g_sums), where=g_counts!=0)
         return V
+    
+    def every_visit(self):
+        V = np.zeros(self.num_states)
+        g_sums = np.zeros(self.num_states)
+        g_counts = np.zeros(self.num_states)
+
+        for episode in self.episodes:
+            G = 0 
+            for t in reversed(range(len(episode))):
+                s, r = episode[t]
+                G = r + self.gamma * G
+                g_sums[s] += G
+                g_counts[s] += 1
+
+        V = np.divide(g_sums, g_counts, out=np.zeros_like(g_sums), where=g_counts!=0)
+        return V
+
+    def incremental(self):
+        V = np.zeros(self.num_states)
+
+        for episode in self.episodes:
+            G = 0
+            for t in reversed(range(len(episode))):
+                s, r = episode[t]
+                G = r + self.gamma * G
+                V[s] += self.alpha * (G - V[s]) 
+
+        return V
